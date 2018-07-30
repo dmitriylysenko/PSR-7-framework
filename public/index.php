c2f44167e55bdb038dc5122f43852d628f93bb07
dmitriy <dilife90@gmail.com>
2018-07-31 01:03:22 +0200

    II lesson - 1:46

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\RouteCollection;
+use Framework\Http\Router\Router;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
 
@@ -9,13 +14,52 @@ require 'vendor/autoload.php';
 
 ### Initialization
 
+$routes = new RouteCollection();
+
+$routes->get('home', '/', function (ServerRequestInterface $request) {
+  $name = $request->getQueryParams()['name'] ?? 'Guest';
+  return new HtmlResponse('Hello ' . $name . '!');
+});
+
+$routes->get('about', '/about', function () {
+  return new HtmlResponse('I am a simple site');
+});
+
+$routes->get('blog', '/blog', function () {
+  return new JsonResponse([
+    ['id' => 2, 'title' => 'The Second Post'],
+    ['id' => 1, 'title' => 'The First Post']
+  ]);
+});
+
+$routes->get('blog_show', '/blog/{id}', function (ServerRequestInterface $request) {
+  $id = $request->getAttribute('id');
+  if ($id > 5) {
+    return new JsonResponse(['error' => 'Undefined page'], 404);
+  }
+  return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+}, ['id' => '\d+']);
+
+$router = new Router($routes);
+
+### Running
+
 $request = ServerRequestFactory::fromGlobals();
+try {
+  $result = $router->match($request);
+  foreach ($result->getAttributes() as $attribute => $value) {
+    $request = $request->withAttribute($attribute, $value);
+  }
+  /** @var callable $action */
+  $action   = $result->getHandler();
+  $response = $action($request);
+} catch (RequestNotMatchedException $e) {
+  $response = new JsonResponse(['error' => 'Undefined page'], 404);
+}
 
-### Action
+###Postprocessing
 
-$name     = $request->getQueryParams()['name'] ?? 'Guest';
-$response = (new HtmlResponse("Hello, $name!"))
-  ->withHeader('X-developer', 'ElisDN');
+$response = $response->withHeader('X-developer', 'ElisDN');
 
 ### Sending
 
